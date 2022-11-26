560250a2de23a71d81c4784b2269a65a9b46c528
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-11-25 22:01:00 -0300

    Initial Commit

---
+++ Dicee/DiceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeApp.swift
+//  Dicee
+//
+//  Created by kenjimaeda on 25/11/22.
+//
+
+import SwiftUI
+
+@main
+struct DiceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
